e4559342fc4162b96b7d59f7278292997328ec7f
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-05-11 17:19:34 +0200

    Generate some tests
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

---
+++ tests/broker/kafka/p10-r3-ord-b100-t10/additional.sh
@@ -0,0 +1,4 @@
+HTTP_TARGET=$(oc get brokers.eventing.knative.dev -n "${TEST_CASE_NAMESPACE}" p10-r3-ord-0 -o jsonpath='{.status.address.url}') || exit 1
+BENCHMARK_NAME=eventing-staircase
+
+export HTTP_TARGET BENCHMARK_NAME
