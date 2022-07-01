d4f24e732ad3f743e1b7321f1324c9f513e769a7
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-07-01 15:14:07 +0200

    Update update-nightlies.sh script to use upstream-nightly for clarity
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- bin/update-nightlies.sh
+++ bin/update-nightlies.sh
@@ -2,15 +2,15 @@
 
 echo "=== Downloading upstream nightly artifacts ==="
 
-rm -rf installation/manifests/nightly/eventing
-rm -rf installation/manifests/nightly/eventing-kafka-broker
+rm -rf installation/manifests/upstream-nightly/eventing
+rm -rf installation/manifests/upstream-nightly/eventing-kafka-broker
 
-wget -P installation/manifests/nightly/eventing-kafka-broker https://storage.googleapis.com/knative-nightly/eventing-kafka-broker/latest/eventing-kafka-broker.yaml
-wget -P installation/manifests/nightly/eventing-kafka-broker https://storage.googleapis.com/knative-nightly/eventing-kafka-broker/latest/eventing-kafka-channel.yaml
-wget -P installation/manifests/nightly/eventing-kafka-broker https://storage.googleapis.com/knative-nightly/eventing-kafka-broker/latest/eventing-kafka-controller.yaml
-wget -P installation/manifests/nightly/eventing-kafka-broker https://storage.googleapis.com/knative-nightly/eventing-kafka-broker/latest/eventing-kafka-source.yaml
-wget -P installation/manifests/nightly/eventing-kafka-broker https://storage.googleapis.com/knative-nightly/eventing-kafka-broker/latest/eventing-kafka-sink.yaml
+wget -P installation/manifests/upstream-nightly/eventing-kafka-broker https://storage.googleapis.com/knative-nightly/eventing-kafka-broker/latest/eventing-kafka-broker.yaml
+wget -P installation/manifests/upstream-nightly/eventing-kafka-broker https://storage.googleapis.com/knative-nightly/eventing-kafka-broker/latest/eventing-kafka-channel.yaml
+wget -P installation/manifests/upstream-nightly/eventing-kafka-broker https://storage.googleapis.com/knative-nightly/eventing-kafka-broker/latest/eventing-kafka-controller.yaml
+wget -P installation/manifests/upstream-nightly/eventing-kafka-broker https://storage.googleapis.com/knative-nightly/eventing-kafka-broker/latest/eventing-kafka-source.yaml
+wget -P installation/manifests/upstream-nightly/eventing-kafka-broker https://storage.googleapis.com/knative-nightly/eventing-kafka-broker/latest/eventing-kafka-sink.yaml
 wget -P
-wget -P installation/manifests/nightly/eventing https://storage.googleapis.com/knative-nightly/eventing/latest/eventing-core.yaml
-wget -P installation/manifests/nightly/eventing https://storage.googleapis.com/knative-nightly/eventing/latest/mt-channel-broker.yaml
-wget -P installation/manifests/nightly/eventing https://storage.googleapis.com/knative-nightly/eventing/latest/eventing-post-install.yaml
+wget -P installation/manifests/upstream-nightly/eventing https://storage.googleapis.com/knative-nightly/eventing/latest/eventing-core.yaml
+wget -P installation/manifests/upstream-nightly/eventing https://storage.googleapis.com/knative-nightly/eventing/latest/mt-channel-broker.yaml
+wget -P installation/manifests/upstream-nightly/eventing https://storage.googleapis.com/knative-nightly/eventing/latest/eventing-post-install.yaml
