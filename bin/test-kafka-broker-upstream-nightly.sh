bb3032c6ab0989fc6c81d3d3e0765c368944cc4e
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-07-06 15:58:30 +0200

    Rename tests with proper naming convention
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- bin/test-kafka-broker-upstream-nightly.sh
+++ bin/test-kafka-broker-upstream-nightly.sh
@@ -3,12 +3,11 @@
 set -euo pipefail
 
 export SKIP_DELETE_RESOURCES=true
-export TEST_CASE=tests/broker/kafka/p10-r3-ordered
-export TEST_CASE_NAMESPACE=perf-test
 
+export TEST_CASE=tests/broker/kafka/p10-r3-ord-b10-t10-64KB
+export TEST_CASE_NAMESPACE=perf-test
 ./bin/run_upstream_nightly.sh || exit 1
 
 kubectl delete ns "${TEST_CASE_NAMESPACE}" || exit 1
-export TEST_CASE=tests/broker/kafka/p10-r3-unordered
-
+export TEST_CASE=tests/broker/kafka/p10-r3-unord-b10-t10-64KB
 ./bin/run_test.sh || exit 1
