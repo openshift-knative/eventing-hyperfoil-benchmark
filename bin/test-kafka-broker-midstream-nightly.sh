54e6a3ecc05a1ad1cf8669e4f9c876bafe7d9b36
Pierangelo Di Pilato <pierdipi@redhat.com>
2022-07-22 15:20:13 +0200

    Install components first
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- bin/test-kafka-broker-midstream-nightly.sh
+++ bin/test-kafka-broker-midstream-nightly.sh
@@ -14,7 +14,8 @@ export TEST_CASE_NAMESPACE=perf-test
 
 kubectl delete ns "${TEST_CASE_NAMESPACE}" --ignore-not-found || exit 1
 export TEST_CASE=tests/broker/kafka/p10-r3-ord-b20-t10-32kb
-./bin/run_test.sh || exit 1
+#./bin/run_test.sh || exit 1
+./bin/run_midstream_nightly.sh || exit 1
 
 kubectl delete ns "${TEST_CASE_NAMESPACE}" --ignore-not-found || exit 1
 export TEST_CASE=tests/broker/kafka/p10-r3-unord-b20-t10-32kb
--- bin/test-kafka-broker-upstream-nightly.sh
+++ bin/test-kafka-broker-upstream-nightly.sh
@@ -6,7 +6,6 @@ export SKIP_DELETE_RESOURCES=true
 export TEST_CASE_NAMESPACE=perf-test
 
 #export TEST_CASE=tests/broker/kafka/p10-r3-ord-b100-t5-5b
-#./bin/run_upstream_nightly.sh || exit 1
 #
 #kubectl delete ns "${TEST_CASE_NAMESPACE}" || exit 1
 #export TEST_CASE=tests/broker/kafka/p10-r3-unord-b100-t5-5b
@@ -14,7 +13,8 @@ export TEST_CASE_NAMESPACE=perf-test
 
 kubectl delete ns "${TEST_CASE_NAMESPACE}" --ignore-not-found || exit 1
 export TEST_CASE=tests/broker/kafka/p10-r3-ord-b20-t10-32kb
-./bin/run_test.sh || exit 1
+# ./bin/run_test.sh || exit 1
+./bin/run_upstream_nightly.sh || exit 1
 
 kubectl delete ns "${TEST_CASE_NAMESPACE}" --ignore-not-found || exit 1
 export TEST_CASE=tests/broker/kafka/p10-r3-unord-b20-t10-32kb
