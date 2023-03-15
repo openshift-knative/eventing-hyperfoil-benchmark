35487d251dc41e76ff37c668e2ed1ee2e627809a
Ali Ok <aliok@redhat.com>
2023-03-15 17:54:25 +0300

    Run all tests

--- bin/test-kafka-broker-midstream-nightly.sh
+++ bin/test-kafka-broker-midstream-nightly.sh
@@ -34,14 +34,14 @@ export TEST_CASE=tests/broker/kafka/p10-r3-ord-b20-t10-32kb-namespaced
 #./bin/run_test.sh || exit 1
 ./bin/run_midstream_nightly.sh || exit 1
 
-#kubectl delete ns "${TEST_CASE_NAMESPACE}" --ignore-not-found || exit 1
-#export TEST_CASE=tests/broker/kafka/p10-r3-unord-b20-t10-32kb-namespaced
-#./bin/run_test.sh || exit 1
-#
-#kubectl delete ns "${TEST_CASE_NAMESPACE}" --ignore-not-found || exit 1
-#export TEST_CASE=tests/broker/kafka/p10-r3-ord-b10-t10-64kb-namespaced
-#./bin/run_test.sh || exit 1
-#
-#kubectl delete ns "${TEST_CASE_NAMESPACE}" --ignore-not-found || exit 1
-#export TEST_CASE=tests/broker/kafka/p10-r3-unord-b10-t10-64kb-namespaced
-#./bin/run_test.sh || exit 1
+kubectl delete ns "${TEST_CASE_NAMESPACE}" --ignore-not-found || exit 1
+export TEST_CASE=tests/broker/kafka/p10-r3-unord-b20-t10-32kb-namespaced
+./bin/run_test.sh || exit 1
+
+kubectl delete ns "${TEST_CASE_NAMESPACE}" --ignore-not-found || exit 1
+export TEST_CASE=tests/broker/kafka/p10-r3-ord-b10-t10-64kb-namespaced
+./bin/run_test.sh || exit 1
+
+kubectl delete ns "${TEST_CASE_NAMESPACE}" --ignore-not-found || exit 1
+export TEST_CASE=tests/broker/kafka/p10-r3-unord-b10-t10-64kb-namespaced
+./bin/run_test.sh || exit 1
