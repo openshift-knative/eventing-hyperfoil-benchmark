f14d073c7e6f1123351f0dde52da9c7578280882
Ali Ok <aliok@redhat.com>
2023-03-17 12:15:37 +0300

    Generate files

---
+++ tests/broker/kafka/p10-r3-unord-b20-t10-32kb-namespaced/additional.sh
@@ -0,0 +1,7 @@
+
+
+HTTP_TARGET=$(oc get brokers.eventing.knative.dev -n "${TEST_CASE_NAMESPACE}" p10-r3-unord-b20-t10-32kb-namespaced-0 -o jsonpath='{.status.address.url}') || exit 1
+BENCHMARK_NAME=eventing-staircase
+
+export HTTP_TARGET BENCHMARK_NAME
+
