c06d69f0472cc21160783db8ff804ed5bacf46d2
Ali Ok <aliok@redhat.com>
2023-03-28 14:23:34 +0300

    Delete all namespaced test cases

--- tests/broker/kafka/p10-r3-unord-b100-t5-5b-namespaced/additional.sh
+++
@@ -1,7 +0,0 @@
-
-
-HTTP_TARGET=$(oc get brokers.eventing.knative.dev -n "${TEST_CASE_NAMESPACE}" p10-r3-unord-b100-t5-5b-namespaced-0 -o jsonpath='{.status.address.url}') || exit 1
-BENCHMARK_NAME=eventing-staircase
-
-export HTTP_TARGET BENCHMARK_NAME
-
