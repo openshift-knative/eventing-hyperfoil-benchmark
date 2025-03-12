45f2889e273b8654227cf04cdf868ccfbc9e55b2
Chris Suszynski <krzysztof.suszynski@gmail.com>
2025-03-12 10:39:29 +0100

    Use hacks directly (#90)

--- hack/generate.sh
+++ hack/generate.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 
 repo_root_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")/..
 
-GO111MODULE=off go get -u github.com/openshift-knative/hack/cmd/generate
-
-$(go env GOPATH)/bin/generate \
+go run github.com/openshift-knative/hack/cmd/generate@latest \
   --root-dir "${repo_root_dir}" \
   --generators dockerfile
